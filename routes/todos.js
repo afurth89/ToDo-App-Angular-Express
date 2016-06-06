cc58541620ce71e7d95f2674d8df6fbbdd97122d
Andrew Furth <andrew.furth@gmail.com>
2016-06-06 14:46:55 -0700

    GET /, PUT and DELETE requests all successfully complete using cURL

--- routes/todos.js
+++ routes/todos.js
@@ -8,9 +8,23 @@ var express = require('express'),
 
 // shorthand for '/todos' - see app.js
 router.route('/')
-  // .get((req, res) => {
-    // res.send("hello")
-  // })
+  // Display all todos
+  .get((req, res) => {
+    knex('todos').select()
+      .then((todos) => {
+        res.format({
+          json: function() {
+            res.send( {todos} )  // Uses object literal notation -> {todos: [{todo1}, {todo2}, etc]}
+            // res.send( todos )  // Just sends back an array of data -> [{todo1}, {todo2}, etc]
+          }
+        })
+      })
+      .catch(function(err) {
+        console.log("The GET ERROR IS...", err)
+      })
+
+  })
+  // Create a todo
   .post((req, res) => {
     knex('todos').insert(req.body.todo)
       .returning('*')
@@ -22,8 +36,41 @@ router.route('/')
         })
       })
       .catch(function(err) {
-        console.log("THE ERROR IS...", err)
+        console.log("The POST ERROR IS...", err)
       })
   });
+
+router.route('/:id')
+  .put((req, res) => {
+    knex('todos').where('id', +req.params.id)
+      .update({content: req.body.todo.content, completed: req.body.todo.completed}) 
+      .returning('*')
+      .then(function(todo) {
+        res.format({
+          json: function() {
+            res.send({todo});
+          }
+        })
+      })
+      .catch(function(err) {
+        console.log("The PATCH ERROR IS...", err)
+      })
+  })
+  .delete((req, res) => {
+    knex('todos')
+      .delete() 
+      .where('id', +req.params.id)
+      .returning('*')
+      .then(function(todo) {
+        res.format({
+          json: function() {
+            res.send( {todo} )
+          }
+        })
+      })
+      .catch(function(err) {
+        console.log("The DELETE ERROR IS...", err)
+      })
+  })
 
 module.exports = router;
