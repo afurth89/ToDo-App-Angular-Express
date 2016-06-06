1a6af5f142ebc0a4b3d33a96457d8d1023ecd8b4
Andrew Furth <andrew.furth@gmail.com>
2016-06-06 11:00:54 -0700

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("angularExpressApp", ["ngRoute"])
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when("/", {
+            templateUrl: "../templates/home.html",
+            controller: "homeController"
+        });
+});
---
+++ public/javascripts/controllers.js
@@ -0,0 +1,5 @@
+app.controller("homeController", homeController)
+
+function homeController() {
+  
+}
